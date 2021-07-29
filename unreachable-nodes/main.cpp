9f7f0a92640748f1d0ded606e5f1a47f87ba414e
Ulrich Ntella <ulrich.creatella@gmail.com>
2021-07-29 12:16:30 +0100

    removeed initialize function and added default values for visited[] to false

--- unreachable-nodes/main.cpp
+++ unreachable-nodes/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+   #include <iostream>
    #include <vector>
     using namespace std;
 
     vector <int> adj[100000];
-    bool visited[100000];
+    bool visited[100000] = { false };
     int ans = 0;
 
     void dfs(int s) {
@@ -15,24 +15,18 @@
         }
     }
 
-    void initialize() {
-        for(int i = 0;i < 10;++i)
-         visited[i] = false;
-    }
-
     int main() {
         int nodes, edges, x, y, head, unreachableNodes = 0;
         cin >> nodes;                       //Number of nodes
         cin >> edges;                       //Number of edges
         for(int i = 0;i < edges;++i) {
          cin >> x >> y;     
-     //Undirected Graph 
+         //Undirected Graph 
          adj[x].push_back(y);                   //Edge from vertex x to vertex y
          adj[y].push_back(x);                   //Edge from vertex y to vertex x
         }
         cin >> head;  
 
-        initialize();                           //Initialize all nodes as not visited
         dfs(head);
 
         
